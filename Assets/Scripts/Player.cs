9585d7b45c384bbe353c78f60a772ddaff3868be
eliasbrucart <eliasbrucart@hardgames.com.ar>
2025-11-22 17:07:06 -0300

    Testing Collision and Trigger

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,27 +4,42 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private int speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject wall;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.wKey.isPressed)
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
         {
             Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
             transform.position += direction * speed * Time.deltaTime;
             Debug.Log("Se aprieta tecla W");
         }
 
-        if (Keyboard.current.sKey.isPressed)
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
         {
             Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
             transform.position -= direction * speed * Time.deltaTime;
             Debug.Log("Se aprieta tecla S");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            Debug.Log("Colisiono con " + collision.gameObject.tag);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("wall"))
+        {
+            Debug.Log("Colisiono con " + other.gameObject.tag);
         }
     }
 }
