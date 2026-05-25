2f6ccd13e11c889fc398548da9a41cefcb515690
elias brucart <eliasbrucart@gmail.com>
2026-05-24 21:36:32 -0300

    Applied singleton to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour{
     [SerializeField] private Player player;
     [SerializeField] public int score;
+
+    static public GameManager instanceGameManager;
+    static public GameManager Instance { get { return instanceGameManager; } }
+
+    public void Awake() {
+        if (instanceGameManager != null && instanceGameManager != this)
+            Destroy(instanceGameManager);
+        else
+            instanceGameManager = this;
+    }
+
     void Start(){
         player.updateScore += UpdateScore;
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,40 +7,44 @@ public class Player : MonoBehaviour
     [SerializeField] private int speed;
     [SerializeField] private GameObject wall;
     public Action updateScore;
-    void Start()
-    {
-        
+    void Start(){
+
     }
 
-    void Update()
-    {
-        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
-        {
+    void Update(){
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed){
             Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
             transform.position += direction * speed * Time.deltaTime;
             Debug.Log("Se aprieta tecla W");
         }
 
-        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
-        {
+        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed){
             Vector3 direction = new Vector3(0.0f, 0.0f, 1.0f);
             transform.position -= direction * speed * Time.deltaTime;
             Debug.Log("Se aprieta tecla S");
         }
+
+        if(Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) {
+            Vector3 direction = new Vector3(-1.0f, 0.0f, 0.0f);
+            transform.position += direction * speed * Time.deltaTime;
+            Debug.Log("Se aprieta tecla A");
+        }
+
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) {
+            Vector3 direction = new Vector3(1.0f, 0.0f, 0.0f);
+            transform.position += direction * speed * Time.deltaTime;
+            Debug.Log("Se aprieta tecla A");
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("wall"))
-        {
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("wall")){
             Debug.Log("Colisiono con " + collision.gameObject.tag);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("wall"))
-        {
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("wall")){
             Debug.Log("Colisiono con " + other.gameObject.tag);
             updateScore.Invoke();
         }
