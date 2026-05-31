47e8a692bf8c55aabdd1071a0144b090ee94afb0
elias brucart <eliasbrucart@gmail.com>
2026-05-31 16:28:23 -0300

    Added prefabs folder

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : MonoBehaviour{
     [SerializeField]private float speed;
     [SerializeField] private Player player;
     private Vector3 direction;
-    void Start(){
+    void Start() {
         player.shootBullet += SetBulletDirection;
-        transform.position = new Vector3(player.transform.position.x, player.transform.localScale.y, player.transform.position.z+0.5f);
+        transform.position = new Vector3(player.transform.position.x, player.transform.localScale.y, player.transform.position.z + 0.5f);
+    }
 
     void Update(){
         UpdateBullet();
